5b1d4d3912e0e9ed60ef54b8de1a2d2d1f6a0782
vladaorlik <vladaorlik@users.noreply.github.com>
2023-01-19 18:05:16 +0300

    5

---
+++ js/task-05.js
@@ -0,0 +1,11 @@
+const refs = {
+    inpurEl: document.querySelector('#name-input'),
+    spanEl: document.querySelector('#name-output'),
+};
+const onInputChange = (event) =>
+    refs.inpurEl.value != ''
+        ? refs.spanEl.textContent = refs.inpurEl.value
+        : refs.spanEl.textContent = "Anonymous";
+
+refs.inpurEl.addEventListener('input', onInputChange);
+
