18d46cd5d9f24eaed1cfb8853296d34b8873fb04
vladaorlik <vladaorlik@users.noreply.github.com>
2023-01-19 19:58:45 +0300

    9

---
+++ js/task-06.js
@@ -0,0 +1,13 @@
+const inputEl = document.querySelector('input#validation-input');
+
+inputEl.addEventListener('blur', onInputBlur);
+
+function onInputBlur(event, onInputBlurRemove) {
+    if (event.currentTarget.value.length === Number(inputEl.dataset.length)) {
+        inputEl.classList.remove('invalid');
+    } else if 
+    (event.currentTarget.value.length !== Number(inputEl.dataset.length)) {
+        inputEl.classList.remove('valid');
+        inputEl.classList.add('invalid');
+    }
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,21 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const bodyEl = document.querySelector('body');
+
+const collorEl = document.querySelector('.change-color');
+console.log(collorEl);
+collorEl.addEventListener('click', changecolor);
+
+
+const spanColor = document.querySelector('.color');
+console.log(spanColor);
+
+const color = getRandomHexColor();
+
+function changecolor() {
+  const color = getRandomHexColor();
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  spanColor.textContent = color
+
+}
